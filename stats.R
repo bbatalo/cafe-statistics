d6daaf6ee97da2201b129bc24e7abe987be075dd
bbatalo <b.batalo@doobinnovation.com>
2018-06-28 12:00:40 +0200

    Setup work directory, libraries and load data

---
+++ stats.R
@@ -0,0 +1,14 @@
+# set working directory to git repository
+# change parameters if needed
+home <- Sys.getenv("USERPFOFILE")
+repo <- "/repos/cafe-statistics"
+setwd(paste(home, repo, sep=""))
+
+# make sure you have readxl installed and loaded
+install.packages("readxl")
+library("readxl")
+
+# load dataset with correct attribute types and NA value parsing
+cafe.data <- read_excel("cafedata.xls", na = "na")
+
+# descriptive statistics for total coffee and soda sales
