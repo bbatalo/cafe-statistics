8ea148ee2621bdc31bf9438cf88e72c187a78865
Bojan Batalo <bbatalo@hotmail.com>
2018-06-30 17:33:22 +0200

    Day desc statistics for sodas

--- stats.R
+++ stats.R
@@ -3,6 +3,7 @@
 home <- Sys.getenv("USERPROFILE")
 repo <- "/repos/cafe-statistics"
 setwd(paste(home, repo, sep=""))
+getwd()
 
 # make sure you have readxl installed and loaded
 install.packages("readxl")
@@ -50,8 +51,41 @@ sodas.na <- c(sum(is.na(cafe.data[cafe.data$`Day Code` == 1, ]$Sodas)),
 names(sodas.na) <- days.ordered
 sodas.mean <- tapply(cafe.data$Sodas, cafe.data$`Day Code`, mean, na.rm=TRUE)
 names(sodas.mean) <- days.ordered
+sodas.stdev <- tapply(cafe.data$Sodas, cafe.data$`Day Code`, sd, na.rm=TRUE)
+names(sodas.stdev) <- days.ordered
+tmp.lengths <- c(length(na.omit(cafe.data[cafe.data$`Day Code` == 1, ]$Sodas)),
+                 length(na.omit(cafe.data[cafe.data$`Day Code` == 2, ]$Sodas)),
+                 length(na.omit(cafe.data[cafe.data$`Day Code` == 3, ]$Sodas)),
+                 length(na.omit(cafe.data[cafe.data$`Day Code` == 4, ]$Sodas)),
+                 length(na.omit(cafe.data[cafe.data$`Day Code` == 5, ]$Sodas)))
+sodas.semean <- sodas.stdev / sqrt(tmp.lengths)
+names(sodas.semean) <- days.ordered
 sodas.min <- tapply(cafe.data$Sodas, cafe.data$`Day Code`, min, na.rm=TRUE)
 names(sodas.min) <- days.ordered
+sodas.quantiles <- tapply(cafe.data$Sodas, cafe.data$`Day Code`, quantile, na.rm=TRUE)
+sodas.q1 <- c(sodas.quantiles$`1`[2],
+              sodas.quantiles$`2`[2],
+              sodas.quantiles$`3`[2],
+              sodas.quantiles$`4`[2],
+              sodas.quantiles$`5`[2])
+names(sodas.q1) <- days.ordered
+sodas.median <- c(sodas.quantiles$`1`[3],
+              sodas.quantiles$`2`[3],
+              sodas.quantiles$`3`[3],
+              sodas.quantiles$`4`[3],
+              sodas.quantiles$`5`[3])
+names(sodas.median) <- days.ordered
+sodas.q3 <- c(sodas.quantiles$`1`[4],
+              sodas.quantiles$`2`[4],
+              sodas.quantiles$`3`[4],
+              sodas.quantiles$`4`[4],
+              sodas.quantiles$`5`[4])
+names(sodas.q3) <- days.ordered
 sodas.max <- tapply(cafe.data$Sodas, cafe.data$`Day Code`, max, na.rm=TRUE)
 names(sodas.max) <- days.ordered
-
+desc.sodas.days <- data.frame(days.ordered, sodas.n, sodas.na, sodas.mean, sodas.semean, sodas.stdev, 
+                              sodas.min, sodas.q1, sodas.median, sodas.q3, sodas.max)
+day.col.names <- col.names <- c('Day Of Week', 'N', 'NA', 'Mean', 'SE Mean', 'StDev', 
+                                'Minimum', 'Q1', 'Median', 'Q3', 'Maximum')
+names(desc.sodas.days) <- day.col.names
+View(desc.sodas.days)
